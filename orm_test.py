2489d9fc1cabd3e5c8ddaffb80d4368932bb20c6
lbja2 <lbja2@live.com>
2019-05-04 10:50:12 +0800

    yew

--- orm_test.py
+++ orm_test.py
@@ -1,14 +1,86 @@
-import asyncio
-import orm
-from models import User, Blog, Comment
-import nest_asyncio
-nest_asyncio.apply()
-async def test(loop):
-    await orm.create_pool(loop=loop,user='root', password='776007', db='awesome')
-    #u = User(name='Test3', email='test@example.com2', #passwd='1234567890', image='about:blank')
-    #await  u.save()
-    await  User().findNumber('id', 'name=Test')
-    print(id)
-loop=asyncio.get_event_loop()
-loop.run_until_complete(test(loop))
+
 #loop.close()
+'''def _odd_iter():
+    n = 1
+    while n<9:
+        n = n + 2
+        yield n
+def _not_divisible(n):
+    def m(x):
+        return x % n > 0
+    return m    
+def primes():
+    yield 2
+    it = _odd_iter() # 初始序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(lambda x: x % n > 0, it)  # 构造新序列
+        
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break 
+l = [x for x in range(10)]
+ 
+j = [x for x in range(10)]
+ 
+ 
+ 
+def func(n):
+    def m(x):
+        return x % n > 0
+    return m   
+    #return lambda x: x != i#筛选出与i不相同的数
+ 
+for i in range(1,10):
+ 
+    j = filter(func(i), j)#不同处
+ 
+for i in range(1,10):
+ 
+    l = filter(lambda x: x % i > 0, l)  #不同处 
+print(list(l))
+ 
+print(list(j))
+'''
+def count():
+    funcs=[]
+    n = 0
+    while n < 10:
+        n += 1
+        def f(i):
+            return i == n
+        funcs.append(f)
+    return funcs
+lam = [x for x in range(10)]
+ 
+j = [x for x in range(10)]
+def func(n):
+    def m(x):
+        return x==n
+    return m   
+    #return lambda x: x != i#筛选出与i不相同的数
+ 
+for i in range(10):
+ 
+    j = filter(func(i), j)  #不同处 
+for i in range(10):
+ 
+    lam = filter(lambda x: x==i, lam)  #不同处 
+print(list(j))
+print(list(lam))
+def E(x, n):
+    print('x:', x, 'n:', n)
+    
+def Md():
+    func=[]
+    for i in range(10):
+        def m():
+            print(i)
+        func.append(m)
+    return func    
+for m in Md():
+    m()
+
